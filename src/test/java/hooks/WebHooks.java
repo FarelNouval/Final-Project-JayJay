e61aca2d6baa7b141eb53af1019b0d9f8bbb7448
FarelNouval <ajahfarel49@gmail.com>
2026-02-24 13:33:27 +0700

    fix CI

--- src/test/java/hooks/WebHooks.java
+++ src/test/java/hooks/WebHooks.java
@@ -4,20 +4,34 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebHooks {
+
     public static WebDriver driver;
 
     @Before("@web")
-    public void setup() {
+    public void setUp() {
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
     }
 
     @After("@web")
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
